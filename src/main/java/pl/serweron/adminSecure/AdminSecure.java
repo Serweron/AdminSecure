5fe18358e7040c2e3373d90b0441d30a6dd121ce
Piotr Józef Kołodziejczyk <124733262+MasnyPen@users.noreply.github.com>
2025-07-23 16:11:49 +0200

    initialize

---
+++ src/main/java/pl/serweron/adminSecure/AdminSecure.java
@@ -0,0 +1,17 @@
+package pl.serweron.adminSecure;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdminSecure extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
